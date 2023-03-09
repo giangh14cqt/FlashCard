59862a5114c756f20c86fcac28e4d356e0d70a0a
Truong Giang Do <giangh14cqt@gmail.com>
2023-03-09 22:06:59 +0100

    Add basic code

---
+++ api/py_object_id.py
@@ -0,0 +1,4 @@
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    pass
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 from fastapi import FastAPI
+import motor.motor_asyncio
 
 
 app = FastAPI()
-
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.college
 
 @app.get("/")
 async def root():
