275acbe72c11eee07a7477ad3ff0014ef5d57371
Truong Giang Do <giangh14cqt@gmail.com>
2023-03-09 22:16:33 +0100

    Add FlashCard model

--- api/flash_card.py
+++ api/flash_card.py
@@ -1,10 +1,39 @@
+from typing import Optional
 from fastapi import APIRouter
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
+from py_object_id import PyObjectId
+from bson import ObjectId
 
 router = APIRouter()
 
 
-class CreateFlashCard(BaseModel):
-    frontSide: str
-    backSide: str
+class FlashCardModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    front_side: str = Field(...)
+    back_side: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "front_side": "Hola",
+                "back_side": "Hello",
+            }
+        }
+
+
+class UpdateFlashCard(BaseModel):
+    front_side: Optional[str]
+    back_side: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "front_side": "Hola",
+                "back_side": "Hello",
+            }
+        }
